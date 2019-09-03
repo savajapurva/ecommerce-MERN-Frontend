f903bf066a4718f3c5ee81a4aacd27ed74aeadff
savajapurva <33167674+savajapurva@users.noreply.github.com>
2019-09-03 00:22:50 -0400

    updated routes

--- src/Routes.js
+++ src/Routes.js
@@ -18,54 +18,31 @@ import ManageProducts from "./admin/ManageProducts";
 import UpdateProduct from "./admin/UpdateProduct";
 
 const Routes = () => {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/shop" exact component={Shop} />
-                <Route path="/signin" exact component={Signin} />
-                <Route path="/signup" exact component={Signup} />
-                <PrivateRoute
-                    path="/user/dashboard"
-                    exact
-                    component={Dashboard}
-                />
-                <AdminRoute
-                    path="/admin/dashboard"
-                    exact
-                    component={AdminDashboard}
-                />
-                <AdminRoute
-                    path="/create/category"
-                    exact
-                    component={AddCategory}
-                />
-                <AdminRoute
-                    path="/create/product"
-                    exact
-                    component={AddProduct}
-                />
-                <Route path="/product/:productId" exact component={Product} />
-                <Route path="/cart" exact component={Cart} />
-                <AdminRoute path="/admin/orders" exact component={Orders} />
-                <PrivateRoute
-                    path="/profile/:userId"
-                    exact
-                    component={Profile}
-                />
-                <PrivateRoute
-                    path="/admin/products"
-                    exact
-                    component={ManageProducts}
-                />
-                <AdminRoute
-                    path="/admin/products"
-                    exact
-                    component={ManageProducts}
-                />
-            </Switch>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/shop" exact component={Shop} />
+        <Route path="/signin" exact component={Signin} />
+        <Route path="/signup" exact component={Signup} />
+        <PrivateRoute path="/user/dashboard" exact component={Dashboard} />
+        <AdminRoute path="/admin/dashboard" exact component={AdminDashboard} />
+        <AdminRoute path="/create/category" exact component={AddCategory} />
+        <AdminRoute path="/create/product" exact component={AddProduct} />
+        <Route path="/product/:productId" exact component={Product} />
+        <Route path="/cart" exact component={Cart} />
+        <AdminRoute path="/admin/orders" exact component={Orders} />
+        <PrivateRoute path="/profile/:userId" exact component={Profile} />
+        <PrivateRoute path="/admin/products" exact component={ManageProducts} />
+        <AdminRoute path="/admin/products" exact component={ManageProducts} />
+        <AdminRoute
+          path="/admin/product/update/:productId"
+          exact
+          component={UpdateProduct}
+        />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default Routes;
